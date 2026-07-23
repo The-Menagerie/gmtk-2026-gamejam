db6f3b4eaaefa2f2cb9b9f7e277777d1d73b0e87
ZanyJake5 <zanyjake5@gmail.com>
2026-07-23 01:21:56 -0400

    Improved Cursors

--- bullet/Scripts/mouse_reticle.gd
+++ bullet/Scripts/mouse_reticle.gd
@@ -5,8 +5,8 @@ var reticle_clicked = load("res://Assets/Tilesets/StrangeCowboy/Player/reticle_c
 var clicked = false
 
 func _ready():
-	Input.set_custom_mouse_cursor(reticle_clicked,Input.CURSOR_FDIAGSIZE)
-	Input.set_custom_mouse_cursor(reticle)
+	Input.set_custom_mouse_cursor(reticle_clicked,Input.CURSOR_FDIAGSIZE,Vector2(20,20))
+	Input.set_custom_mouse_cursor(reticle,Input.CURSOR_ARROW,Vector2(20,20))
 
 
 func _input(event: InputEvent) -> void:
