2c8655f64ce137eb269363b9f023e295eb9abe5d
ZanyJake5 <zanyjake5@gmail.com>
2026-07-23 00:08:37 -0400

    Circular Debug Room :)

--- bullet/Scripts/enemy.gd
+++ bullet/Scripts/enemy.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var gravity : float = 900.0
 
+signal target_destroyed(target)
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
