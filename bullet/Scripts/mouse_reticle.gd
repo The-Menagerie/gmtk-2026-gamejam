e5274cc6201cdf8bbfe025f2e0afb6498ba9db0d
ZanyJake5 <zanyjake5@gmail.com>
2026-07-23 01:25:19 -0400

    Sound

--- bullet/Scripts/mouse_reticle.gd
+++ bullet/Scripts/mouse_reticle.gd
@@ -5,8 +5,8 @@ var reticle_clicked = load("res://Assets/Tilesets/StrangeCowboy/Player/reticle_c
 var clicked = false
 
 func _ready():
-	Input.set_custom_mouse_cursor(reticle_clicked,Input.CURSOR_FDIAGSIZE,Vector2(20,20))
-	Input.set_custom_mouse_cursor(reticle,Input.CURSOR_ARROW,Vector2(20,20))
+	Input.set_custom_mouse_cursor(reticle_clicked,Input.CURSOR_FDIAGSIZE,Vector2(10,10))
+	Input.set_custom_mouse_cursor(reticle,Input.CURSOR_ARROW,Vector2(10,10))
 
 
 func _input(event: InputEvent) -> void:
