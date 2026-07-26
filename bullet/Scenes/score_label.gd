2aa480a50fc0db26a559fb42199c53ff1faf6f08
AgentWebFoot <etg1717@gmail.com>
2026-07-25 22:05:14 -0400

    Changes to how score works

--- bullet/Scenes/score_label.gd
+++ bullet/Scenes/score_label.gd
@@ -1,11 +1,41 @@
 extends Label
 
-var score: int = 1000
+var score: int = 0
+var score_enabled := false
 
 func _ready() -> void:
 	ScoreBus.score_update.connect(update_score)
+	hide()
+	text = "Score: %d" % score
+
+	var game_manager := get_tree().root.find_child("MainGame", true, false)
+	if game_manager != null and game_manager.has_signal("level_changed"):
+		game_manager.level_changed.connect(_on_level_changed)
+		_on_level_changed(game_manager.current_level.scene_file_path)
 
 func update_score(score_change: int) -> void:
+	if not score_enabled:
+		return
+
 	score += score_change
 	score = max(score, 0)
 	text = "Score: %d" % score
+
+func _on_level_changed(level_path: String) -> void:
+	var level_name := level_path.get_file()
+
+	if level_name == "lvl_01.tscn":
+		score = ScoreBus.starting_score
+		score_enabled = true
+		show()
+		text = "Score: %d" % score
+		return
+
+	if level_name.begins_with("lvl_"):
+		score_enabled = true
+		show()
+		text = "Score: %d" % score
+		return
+
+	score_enabled = false
+	hide()
--- bullet/Scripts/score_bus.gd
+++ bullet/Scripts/score_bus.gd
@@ -1,3 +1,33 @@
 extends Node
 
 signal score_update(score_change) ##Positive score change value
+
+var starting_score: int = 777777
+var score_per_shot: int = -500
+var score_on_enemy_bullet_hit: int = -100
+var score_on_spike_death: int = -500
+var passive_score_loss_per_second: int = 1
+
+var _passive_score_timer: float = 0.0
+
+func _process(delta: float) -> void:
+	if passive_score_loss_per_second <= 0:
+		return
+
+	_passive_score_timer += delta
+
+	while _passive_score_timer >= 1.0:
+		_passive_score_timer -= 1.0
+		score_update.emit(-passive_score_loss_per_second)
+
+func reset_score() -> void:
+	score_update.emit(starting_score)
+
+func player_fired_shot() -> void:
+	score_update.emit(score_per_shot)
+
+func player_hit_by_enemy_bullet() -> void:
+	score_update.emit(score_on_enemy_bullet_hit)
+
+func player_died_to_spikes() -> void:
+	score_update.emit(score_on_spike_death)
