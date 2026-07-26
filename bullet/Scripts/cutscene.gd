21234fc626facf09c11947da34980e8c0cbe1eac
ZanyJake5 <zanyjake5@gmail.com>
2026-07-26 05:22:30 -0400

    It fixed it ig?

--- bullet/Scripts/cutscene.gd
+++ bullet/Scripts/cutscene.gd
@@ -1,10 +1,16 @@
 extends Control
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var continue_button: Button = $Button
-@onready var reveal_masks: Control = $RevealMasks
+@onready var animation_player: AnimationPlayer = $ScreenAdjuster/AnimationPlayer
+@onready var continue_button: Button = $ScreenAdjuster/Button
+@onready var reveal_masks: Control = $ScreenAdjuster/RevealMasks
+@onready var adjuster: Node2D = $ScreenAdjuster
+
+var last_known_size = 1080
 
 func _ready() -> void:
+	
+	
+	
 	if is_instance_valid(animation_player) and animation_player.has_animation("Cutscene"):
 		animation_player.play("Cutscene")
 
@@ -18,3 +24,12 @@ func _ready() -> void:
 func _on_continue_pressed() -> void:
 	MusicManager.stop_music()
 	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
+	
+
+func _process(delta: float) -> void:
+	if self.size.y != last_known_size:
+		last_known_size = self.size.y
+		print(last_known_size)
+		var adjustment_distance = (self.size.y - last_known_size)/2
+		adjuster.position.y += adjustment_distance
+	
